498db16f3216fa3d108d658627abf04c7d960cc6
canielcm <ccastromaussa@gmail.com>
2021-12-12 16:07:19 -0500

    nueva estructura

--- lib/domain/controller/Authcontroller.dart
+++ lib/domain/controller/Authcontroller.dart
@@ -1,4 +1,3 @@
-
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_1/domain/models/user.dart';
@@ -8,25 +7,60 @@ class AuthController extends GetxController {
   void updateUser(String user) {
     email.value = user;
   }
+
   void reset() {
     email.value = "";
     //   text.value = "0";
   }
 
   //*************************TRUE CODE*********************************
-//  final FirebaseAuth _auth = FirebaseAuth.instance;
-//  User _userFromFirebaseUser(FirebaseUser user){} 
+  Future<void> login(theEmail, thePassword) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: theEmail, password: thePassword);
+      print("ok");
+      return Future.value(true);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print("NOK 1");
+        return Future.error("User not found");
+      } else if (e.code == 'wrong password') {
+        print('NOK 2');
+        return Future.error('wrong password');
+      }
+    }
+    print('NOK');
+  }
 
+  Future<void> signUp(email, password) async {
+    try {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return Future.value(true);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        return Future.error('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        return Future.error('The account already exists for that email.');
+      }
+    }
+  }
 
-// sign in as Anon
-// Future signInAnon()async {
-//   try{
+  Future<void> logOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      return Future.error(e.toString());
+    }
+  }
 
-//   }catch(e){
+  String userEmail() {
+    String email = FirebaseAuth.instance.currentUser!.email ?? "a@a.com";
+    return email;
+  }
 
-//   }
-// }
-  // sign in 
-  
-
+  String getUid() {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    return uid;
+  }
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,42 +1,40 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/domain/controller/Authcontroller.dart';
 import 'package:flutter_application_1/src/pages/chat.dart';
 import 'package:flutter_application_1/src/pages/feed_page.dart';
 import 'package:flutter_application_1/src/pages/home_page.dart';
 import 'package:flutter_application_1/src/pages/register_page.dart';
 import 'pages/feed2_page.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   final Future<FirebaseApp> _inizialization = Firebase.initializeApp();
   @override
   Widget build(context) {
     return MaterialApp(
-
-        // home: Center(
-        //   child: HomePage(),
-        // ),
-        // home: FutureBuilder(
-        //   future: _inizialization,
-        //   builder: (BuildContext context, AsyncSnapshot snapshot) {
-        //     if(snapshot.hasError){
-        //       print('error ${snapshot.error}');
-
-        //     }
-        //   },
-        // ),
-        home: Scaffold(
-      body: FutureBuilder(
+        home: Container(
+      child: FutureBuilder(
           future: _inizialization,
           builder: (context, snapshot) {
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              initialRoute: '/',
-              routes: <String, WidgetBuilder>{
-                '/': (BuildContext context) => HomePage(),
-                'register': (BuildContext context) => RegisterPage(),
-                'feed2': (BuildContext context) => Feed2(),
-                'chat': (BuildContext context) => Chat(),
-              },
+            if (snapshot.hasError) {
+              return Text('error: ${snapshot.error}');
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              Get.put(AuthController());
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                initialRoute: '/',
+                routes: <String, WidgetBuilder>{
+                  '/': (BuildContext context) => HomePage(),
+                  'register': (BuildContext context) => RegisterPage(),
+                  'feed2': (BuildContext context) => Feed2(),
+                  'chat': (BuildContext context) => Chat(),
+                },
+              );
+            }
+            return Center(
+              child: Text('Loading...'),
             );
           }),
     ));
