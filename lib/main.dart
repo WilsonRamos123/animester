31db13b969bc1d5e8f26f56ab1816adfa4ed9e95
canielcm <ccastromaussa@gmail.com>
2021-12-15 01:53:07 -0500

    chat is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/domain/controller/Chatcontroller.dart';
 import 'package:flutter_application_1/domain/controller/Firestorecontroller.dart';
 import 'package:flutter_application_1/src/app.dart';
 import 'package:get/get.dart';
@@ -10,5 +11,7 @@ void main() {
   Get.lazyPut<TextController>(() => TextController());
   Get.lazyPut<AuthController>(() => AuthController());
   Get.lazyPut<FirebaseController>(() => FirebaseController());
+  Get.lazyPut<ChatController>(() => ChatController());
+  
   runApp(new MyApp());
 }
